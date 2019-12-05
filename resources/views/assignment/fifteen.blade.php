898270be76096d6d7f67372059031ce057a5deec
riyana92 <riyanamol@gmail.com>
2019-12-05 10:05:30 +0530

    edited

--- resources/views/assignment/fifteen.blade.php
+++ resources/views/assignment/fifteen.blade.php
@@ -1,36 +1,55 @@
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>ASSIGNMENT 15</title> 
 </head>
-<body bgcolor="light blue">
-	<h1>STEP 1 
-		<span style="font-size:15px">All fields are mandatory</span> </h1>
-	<h3>Personal Information</h3>
-	<form>
-		<label> First Name:</label>
-		<input type="text" name="Fname">
-		<label>Last Name:</label>
-		<input type="text" name="Sname">
-		<br>
-		<label>E-mail:</label>
-		<input type="E-mail" name="Email">
-		<label>Tel Number:</label>
-		<input type="text" name="tnumber">
-		<br>
-		<label>Mobile Number:<label>
-		<input type="text" name="Mnumber">
-		<label>City</label>
-		<input type="text" name="city">
-		<br>
-		<label>Gender</label>
-		<input type="radio" name="gender" value="male">
-		<label >Male</label>
-		<input type="radio" name="gender" value="female">
-		<label>Female</label>
-		<br>
-	<label>Date Of Birth</label>
-		<select name="day">
+
+<body bgcolor="lightskyblue"> 
+	<table border="0" width="1100px" >
+		<tr>
+			<td colspan="6"><h2>STEP 1</h2></td>
+		</tr>
+		<tr>
+			<td colspan="6">
+				<table width="1100px" cellspacing="0">
+				 <td colspan="3" bgcolor="white"><b>PERSONAL INFORMATION</b></td>
+				 <td colspan="3" font color="black" font size="2px" align="right" bgcolor="white">All feilds are mandatory</td>
+				    </tr>
+				</table>
+
+		</tr>	
+		<tr>
+			<td><label> First Name:</label></td>
+			<td colspan="2"><input type="text" size="50px"name="Fname"></td>
+			<td><label>Last Name:</label></td>
+			<td colspan="2"><input type="text" size="50px" name="Sname"></td>
+		</tr>	
+		<tr>
+			<td><label>E-mail:</label></td>
+			<td colspan="2"><input type="E-mail" name="Email" size="50"></td>
+			<td><label>Tel Number:</label></td>
+			<td colspan="2"><input type="text" name="tnumber" size="50"></td>
+		</tr>
+		<tr>
+			<td><label>Mobile Number:</label></td>
+			<td colspan="2"><input type="text" name="Mnumber" size="50"></td>
+			<td><label>City</label></td>
+			<td colspan="2"><input type="text" name="city" size="50"></td>
+		</tr>
+		<tr>
+			<td><label>State:</label></td>
+			<td colspan="2"><input type="text" name="state" size="50"></td>	
+		    <td><label>Gender</label></td>
+		    <td colspan="2"><input type="radio" name="gender" value="male">    
+		    <label >Male</label>
+		    <input type="radio" name="gender" value="female">
+  			<label>Female</label></td>
+  		</tr>
+  		<tr>		
+			<td>Date Of Birth</td>
+			<td colspan="2"><select name="day" width="25">
+			<option value="day">Day</option>>	
 			<option value="1">1</option>
 			<option value="2">2</option>
 			<option value="3">3</option>
@@ -64,6 +83,7 @@
 			<option value="31">31</option>
 		</select>
 		<select name="month">
+			<option value="month">Month</option>
 			<option value="jan">january</option>
 			<option value="feb">february</option>
 			<option value="mar">march</option>
@@ -78,6 +98,7 @@
 			<option value="dec">december</option>
 		</select>
 		<select>
+			<option value="year">Year</option>
 			<option value="90">1990</option>
 			<option value="91">1991</option>
 			<option value="92">1992</option>
@@ -110,77 +131,112 @@
 			<option value="19">2019</option>
 
 
-		</select>	
-	<label>Emergency Contact Number:</label>
-	<input type="text" name="enumber">
-	<br>
-	<label>Name:</label>
-	<input type="text" name="ename">
-	<label>Relationship:</label>
-	<input type="text" name="relation">
-	<br>
-	<label> Mobile Number:</label>
-	<input type="text" name="emnuber">
-	<br>
-	</form>
-	<h1>STEP 2<span style="font-size:15px">All fields are mandatory</span></h1>
-	<h3>Other Details</h3>
-	<label>Employment Details:</label>
-	<input type="radio" name="edet" value="Public Sector">
-	<label>Public Sector</label>
-	<input type="radio" name="edet" value="Student" checked="yes">
-	<label>Student</label>
-	<input type="radio" name="edet" value="SelfEmployed">
-	<label>SelfEmployed</label>
-	<input type="radio" name="edet" value="Others">
-	<label>Others</label>
-	<br>
-	<label>Annual Family Income</label>
-	<input type="radio" name="income" value="1" checked="yes">
-    <label>50,000</label>
-    <input type="radio" name="income" value="2">
-    <label>50,000-1,00000</label>
-    <input type="radio" name="income" value="3">
-    <label>1lakh-5lakh</label>
-    <input type="radio" name="income" value="4">
-    <label>5lakh-10lakh</label>
-    <input type="radio" name="income" value="5">
-    <label>Above 10Lakh>
-    <br>
+		</select></td>	
+		<td><label>Emergency Contact Number:</label></td>
+		<td colspan="2"><input type="text" name="enumber" size="50"></td>
+	</tr>
+	<tr>
+		<td><label>Name:</label></td>
+		<td colspan="2"><input type="text" name="ename" size="50"></td>
+		<td><label>Relationship:</label></td>
+		<td colspan="2"><input type="text" name="relation" size="50"></td>
+	</tr>
+	<tr>
+		<td><label> Mobile Number:</label></td>
+		<td colspan="2"><input type="text" name="emnuber" size="50"></td>
+	</tr>
+	<tr>
+		<td colspan="6"><h2>STEP 2</h2></td>
+	</tr>	
+	<tr>
+		<td colspan=6>
+			<table width="1100px" cellspacing="0">
+				<td colspan="3" bgcolor="white"><b>OTHER DETAILS</b></td>
+				<td colspan="3" align="right" bgcolor="white"><font color="black" font size="2">All fields are mandatory</font></td>
+			</table>
+		</td>	
+	</tr>
+	<tr>
+		<td>
+	    <label>Employment Details:</label>
+		</td>
+		<td><input type="radio" name="edet" value="Public Sector">
+		<label>Public Sector</label></td>
+		<td><input type="radio" name="edet" value="Student" checked="yes">
+		<label>Student</label></td>
+		<td>
+		<input type="radio" name="edet" value="SelfEmployed">
+		<label>SelfEmployed</label></td>
+		<td><input type="radio" name="edet" value="Others">
+		<label>Others</label>
+		</td>
+	</tr>	
+	<tr>
 
-	<label>Education:</label>    
-	<input type="radio" name="educ" value="1">
-	<label>high school</label>
-	<input type="radio" name="educ" value="2">
-	<label>Graduate</label>
-	<input type="radio" name="educ" value="3">
-	<label>PostGraduate</label>
-	<input type="radio" name="educ" value="4">
-	<label>Professional</label>
-	<input type="radio" name="educ" value="5" checked="yes">
-	<label>Others</label>
-	<br>
-	<label>I Own The Following</label>
-	<input type="checkbox" name="car" value="c">
-	<label>car</label>
-	<input type="checkbox" name="credit card" value="cc">
-    <label>credit card</label>
-	<input type="checkbox" name="mp" value="mp">
-	<label>mobile phone</label>
-	<input type="checkbox" name="cm" value="cm">
-	<label>club membership</label>
-	<input type="checkbox" name="ip" value=ip"">
-	<label>Insurence Policy</label>
-	<br>
-	<label>Cycling Experience</label>
-	<input type="checkbox" name="reg" checked="yes">
-	<label>I am a regular cyclist</label>
-	<input type="Checkbox" name="reg" checked="yes">
-	<label>I am a part of a cycling club</label>
-	<br>
-	<label>Name of the cycing club</label>
-	<input type="text" name="ccname">
-	<h1>STEP 3</h1>
-	<h3>Medical condition<span style="font-size:15px">All fields are mandatory</span></h3>
+		<td><label>Annual Family Income</label></td>
+		<td><input type="radio" name="income" value="1" checked="yes">
+	    <label>50,000</label></td>
+    	<td><input type="radio" name="income" value="2">
+    	<label>50,000-1,00000</label></td>
+    	<td><input type="radio" name="income" value="3">
+    	<label>1lakh-5lakh</label></td>
+    	<td>
+    	<input type="radio" name="income" value="4">
+    	<label>5lakh-10lakh</label></td>
+    	<td>
+    	<input type="radio" name="income" value="5">
+    	<label>Above 10Lakh></label></td>
+    </tr>
+    <tr>
+		<td><label>Education:</label></td>    
+		<td><input type="radio" name="educ" value="1">
+		<label>high school</label></td>
+		<td><input type="radio" name="educ" value="2">
+		<label>Graduate</label></td>
+		<td><input type="radio" name="educ" value="3">
+		<label>PostGraduate</label></td>
+		<td><input type="radio" name="educ" value="4">
+		<label>Professional</label></td>
+		<td><input type="radio" name="educ" value="5" checked="yes">
+	 	<label>Others</label></td>
+	</tr>
+	<tr> 	
+		<td><label>I Own The Following:</label></td>
+		<td><input type="checkbox" name="car" value="c">
+		<label>car</label></td>
+		<td><input type="checkbox" name="credit card" value="cc">
+    	<label>credit card</label></td>
+		<td><input type="checkbox" name="mp" value="mp">
+		<label>mobile phone</label></td>
+		<td><input type="checkbox" name="cm" value="cm">
+		<label>club membership</label></td>
+		<td><input type="checkbox" name="ip" value=ip"">
+		<label>Insurence Policy</label></td>
+	</tr>
+	<tr>
+		<td><label>Cycling Experience:</label></td>
+		<td colspan="3"><input type="checkbox" name="reg" checked="yes">
+		<label>I am a regular cyclist</label>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+		<input type="Checkbox" name="reg" checked="yes">
+		<label>I am a part of a cycling club</label>
+		</td>
+	</tr>
+	<tr>	
+		<td><label>Name of the cycing club:</label></td>
+		<td colspan="2"><input type="text" name="ccname" size="50"></td>
+	<tr>
+		<td colspan="6">	
+		<h2>STEP 3</h2>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="6">
+			<table width="1100px" cellspacing="0">
+				<td colspan="3" bgcolor="white"><b>MEDICAL CONDITION</b></td>
+				<td colspan="3" align="right" bgcolor="white"><font color="black" font size="2">All fields are mandatory</font></td>
+			</table>
+		</td>		
+	</tr>	
+</table>
 </body>
 </html>
--- resources/views/assignment/fourteen.blade.php
+++ resources/views/assignment/fourteen.blade.php
@@ -7,9 +7,9 @@
 	<br><br>
 	<h3><u><span style="color:purple"><a href="{{asset('images/g.png')}}" target="logo">Google</a></span></u></h3>
 	<iframe name="logo" align= "right" width="1000" height="500"></iframe><br><br>
-	<h3><u><span style="color:purple"><a  href="f.jfif" target="logo">Facebook</a></span></u></h3><br><br>
-	<h3><u><span style="color:purple"><a href="t.png" target="logo">Twitter</a></span></u></h3><br><br>
-	<h3><u><span style="color:purple"><a href="m.png" target="logo">Microsoft</a></span></u></h3>
+	<h3><u><span style="color:purple"><a  href="{{asset('images/f.jfif')}}" target="logo">Facebook</a></span></u></h3><br><br>
+	<h3><u><span style="color:purple"><a href="{{asset('images/t.png')}}" target="logo">Twitter</a></span></u></h3><br><br>
+	<h3><u><span style="color:purple"><a href="{{asset('images/m.png')}}" target="logo">Microsoft</a></span></u></h3>
 
 </body>
 </html>
